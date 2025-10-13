6df5c1502e2dcb328233c375b12fb5e7d95aa8c0
Pcy0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2025-10-13 20:40:48 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1348 ms, Memory: 170356 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let answer = "";
+
+for (i = 1; i <= input[0]; i++) {
+    let [a, b] = input[i].split(" ").map((value) => +value);
+    answer += a + b + "\n";
+}
+
+console.log(answer);
