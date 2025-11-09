e6e43319dda85106c95a08a56f491fa36be0fe10
Pcy0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2025-11-09 20:20:14 +0900

    [Bronze III] Title: 팩토리얼, Time: 88 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+function factorial(num){
+    if(num <= 1) return 1;
+    return num * factorial(num - 1);
+}
+console.log(factorial(input));
