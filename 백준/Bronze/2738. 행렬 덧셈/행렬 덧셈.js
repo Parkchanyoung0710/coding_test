89936d2965d12a7ce76b71d819411b9d576615c3
Pcy0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2025-11-11 17:43:23 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 172 ms, Memory: 12520 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [n, m] = inputs[0].split(' ').map(Number);
+
+const A = inputs.slice(1, n + 1).map((it) => it.split(' ').map(Number));
+const B = inputs.slice(n + 1).map((it) => it.split(' ').map(Number));
+
+for (let i = 0; i < n; i++) {
+  const sum = [];
+  for (let j = 0; j < m; j++) {
+    sum.push(A[i][j] + B[i][j]);
+  }
+  console.log(...sum);
+}
