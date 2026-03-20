460dd5c43e9a39610d78463ed1ee4ef121813e7c
Pcy0710 <120624055+Parkchanyoung0710@users.noreply.github.com>
2026-03-20 22:27:38 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    const result1 = String(a) + String(b)
+    const result2 = String(b) + String(a)
+    
+    if(result1 >= result2){
+        return Number(result1);
+    }else{
+        return Number(result2);
+    }
+    return answer;
+}
